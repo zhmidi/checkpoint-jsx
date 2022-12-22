7d58ffbd01bb3d6afcffbaa1f9b49da0735ba4b8
zhmidi <zakaria1997.zh@gmail.com>
2022-12-22 12:08:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className='pic' src='https://media-exp1.licdn.com/dms/image/C4D03AQHIecQq_f9VvQ/profile-displayphoto-shrink_800_800/0/1632432454700?e=2147483647&v=beta&t=92u0XM686CUTm98DR9P96qQuH48yf7i9M90HjeT4FcA'></img> 
+      <h3>Zakaria Hmidi</h3>
+      <p>328 lotissement lafarge, Bouskoura</p>
       </header>
     </div>
   );
